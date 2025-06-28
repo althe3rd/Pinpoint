d2d14639b6a9516aa4a81535528380b3d0726c45
Al Nemec <al.nemec@wisc.edu>
2025-06-27 23:22:35 -0500

    Release 1.3.1

--- accessibility-checker.js
+++ accessibility-checker.js
@@ -9,7 +9,7 @@
     
             // Main accessibility checker object
         window.uwAccessibilityChecker = {
-            version: '1.3.0', // Current version
+            version: '1.3.1', // Current version
             issues: [],
             axeLoaded: false,
             checkedItems: new Set(), // Track manually verified items
